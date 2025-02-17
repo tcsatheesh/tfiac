592fac94c3e99b679160b062d94ad054f49f6a94
Satheesh TC <tc.satheesh@microsoft.com>
2025-02-17 13:49:06 +0000

    updated vnet

--- terraform/network/network.tf
+++ terraform/network/network.tf
@@ -1,25 +1,23 @@
-variable "virtual_network_resource_group_name" {
+variable "market" {
   type = string
 }
-variable "virtual_network_name" {
+variable "environment" {
   type = string
 }
-variable "subnet_name" {
-  type = string
+variable "env_type" {
+  type    = string
+  default = "npd"
+  validation {
+    condition     = var.env_type == "prd" || var.env_type == "npd"
+    error_message = "env_type must be either prd or npd"
+  }
 }
-variable "location" {
-  type = string
-}
-variable "enable_telemetry" {
-  type = bool
-  default = true
-}
-variable "virtual_network_address_space" {
-  type = list(string)
-}
-variable "subnet_address_prefixes" {
-  type = list(string)
-  
+
+locals {
+  dns      = yamldecode(file("../../variables/global/prd/dns.yaml"))
+  log      = yamldecode(file("../../variables/global/${var.env_type}/log.yaml"))
+  vnet     = yamldecode(file("../../variables/${var.market}/${var.env_type}/vnet.yaml"))
+  services = yamldecode(file("../../variables/${var.market}/${var.environment}/services.yaml"))
 }
 
 terraform {
@@ -40,18 +38,41 @@ provider "azurerm" {
   }
 }
 
+provider "azurerm" {
+  features {}
+  alias           = "log_analytics_workspace"
+  subscription_id = local.log.log_analytics_workspace_subscription_id
+}
+
+provider "azurerm" {
+  features {}
+  alias           = "private_dns"
+  subscription_id = local.dns.dns_subscription_id
+}
+
 # This is required for resource modules
-resource "azurerm_resource_group" "this" {
-  location = var.location
-  name     = var.virtual_network_resource_group_name
+data "azurerm_resource_group" "this" {
+  name = local.vnet.virtual_network_resource_group_name
+}
+
+data "azurerm_private_dns_zone" "this" {
+  provider            = azurerm.private_dns
+  name                = local.dns.keyvault_private_dns_zone_name
+  resource_group_name = local.dns.dns_resource_group_name
+}
+
+data "azurerm_log_analytics_workspace" "this" {
+  provider            = azurerm.log_analytics_workspace
+  name                = local.log.log_analytics_workspace_name
+  resource_group_name = local.log.log_analytics_workspace_resource_group_name
 }
 
 # Creating a virtual network with a unique name, telemetry settings, and in the specified resource group and location.
 module "vnet" {
   source              = "Azure/avm-res-network-virtualnetwork/azurerm"
-  name                = var.virtual_network_name
-  enable_telemetry    = var.enable_telemetry
-  resource_group_name = azurerm_resource_group.this.name
-  location            = azurerm_resource_group.this.location
-  address_space       = var.virtual_network_address_space
+  name                = local.vnet.virtual_network_name
+  enable_telemetry    = local.vnet.enable_telemetry
+  resource_group_name = local.vnet.virtual_network_resource_group_name
+  location            = local.vnet.virtual_network_location
+  address_space       = local.vnet.virtual_network_address_space
 }
