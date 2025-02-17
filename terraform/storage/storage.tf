065e6ac121858219b23fd86fccaa433ecc00a552
Satheesh TC <tc.satheesh@microsoft.com>
2025-02-17 17:08:59 +0000

    fix random

--- terraform/storage/storage.tf
+++ terraform/storage/storage.tf
@@ -35,10 +35,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 3.7.0, < 5.0.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.5.0, < 4.0.0"
-    }
   }
 }
 
