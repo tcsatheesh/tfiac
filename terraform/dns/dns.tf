f941d7163d5c25a80aa62e63dc2b7684b8f68887
Satheesh TC <tc.satheesh@microsoft.com>
2025-02-18 12:34:04 +0000

    updated dns

--- terraform/dns/dns.tf
+++ terraform/dns/dns.tf
@@ -1,41 +1,24 @@
-variable "location" {
-    type = string
-}
-variable "dns_subscription_id" {
-    type = string
-  
-}
-variable "dns_resource_group_name" {
-    type = string
-}
-variable "enable_telemetry" {
-    type = bool
-    default = true
-  
-}
-variable "domain_name" {
-    type = string
-  
+locals {
+  dns      = yamldecode(file("../../variables/global/prd/dns.yaml"))
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = var.dns_subscription_id
+  subscription_id = local.dns.dns_subscription_id
 }
 
 # create the resource group
 resource "azurerm_resource_group" "avmrg" {
-  location = var.location
-  name     = var.dns_resource_group_name
+  location = local.dns.dns_location
+  name     = local.dns.dns_resource_group_name
 }
 
 # reference the module and pass in variables as needed
 module "private_dns_zones" {
+  for_each = tomap(local.dns.domain_names)
   # replace source with the correct link to the private_dns_zones module
   source                = "Azure/avm-res-network-privatednszone/azurerm"  
-#   source                = "../"
-  enable_telemetry      = var.enable_telemetry
+  enable_telemetry      = local.dns.enable_telemetry
   resource_group_name   = azurerm_resource_group.avmrg.name
-  domain_name           = var.domain_name
-
+  domain_name           = each.value
 }
