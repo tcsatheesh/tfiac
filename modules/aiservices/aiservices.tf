3b351a2c61612562e821d32640c426f8c6ae1868
Satheesh TC <tc.satheesh@microsoft.com>
2025-02-23 17:01:12 +0000

    fix typo

--- modules/aiservices/aiservices.tf
+++ modules/aiservices/aiservices.tf
@@ -72,7 +72,7 @@ module "aiservices" {
       name                          = "pe-${endpoint}-${var.services.ai_services_name}"
       subnet_resource_id            = data.azurerm_subnet.this.id
       subresource_name              = "account"
-      private_dns_zone_resource_ids = [data.azurerm_private_dns_zone.this.id]
+      private_dns_zone_resource_ids = [data.azurerm_private_dns_zone.this[endpoint].id]
       # these are optional but illustrate making well-aligned service connection & NIC names.
       private_service_connection_name = "psc-${endpoint}-${var.services.ai_services_name}"
       network_interface_name          = "nic-pe-${endpoint}-${var.services.ai_services_name}"
