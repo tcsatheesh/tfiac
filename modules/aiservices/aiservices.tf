5e1e14eb7b4339f196f6b84e53dafe30cd78ba9d
Satheesh TC <tc.satheesh@microsoft.com>
2025-02-23 23:06:52 +0000

    change link endpoints

--- modules/aiservices/aiservices.tf
+++ modules/aiservices/aiservices.tf
@@ -3,6 +3,9 @@ variable "log" {}
 variable "vnet" {}
 variable "services" {}
 
+locals {
+  link_endpoints = toset(["aiservices", "cognitiveservices"])
+}
 
 locals {
   endpoints = toset(["aiservices", "cognitiveservices", "openai"])
@@ -62,7 +65,7 @@ data "azurerm_log_analytics_workspace" "this" {
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "link" {
-  for_each              = local.endpoints
+  for_each              = local.link_endpoints
   provider              = azurerm.private_dns
   name                  = "${each.key}-${var.services.ai_services_name}"
   private_dns_zone_name = var.dns.domain_names[each.key]
