b94aafa3ac377727e7072986f0e2913abccac029
Satheesh TC <tc.satheesh@microsoft.com>
2025-02-24 21:21:36 +0000

    lo

--- terraform/services/main.tf
+++ terraform/services/main.tf
@@ -124,12 +124,12 @@ module "search" {
   services = local.services
 }
 
-# module "logic_app" {
-#   source                           = "../../modules/lgapp"
-#   dns                              = local.dns
-#   log                              = local.log
-#   vnet                             = local.vnet
-#   services                         = local.services
-#   app_insights_instrumentation_key = module.appinsights.instrumentation_key
-#   app_insights_connection_string   = module.appinsights.connection_string
-# }
+module "logic_app" {
+  source                           = "../../modules/lgapp"
+  dns                              = local.dns
+  log                              = local.log
+  vnet                             = local.vnet
+  services                         = local.services
+  app_insights_instrumentation_key = module.appinsights.instrumentation_key
+  app_insights_connection_string   = module.appinsights.connection_string
+}
