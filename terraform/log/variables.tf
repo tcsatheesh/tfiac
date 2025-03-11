fa3aafb403bf6d3d9e408a14979da6fd0f7f096c
Satheesh TC <tc.satheesh@microsoft.com>
2025-03-11 20:39:05 +0000

    format file

--- terraform/log/variables.tf
+++ terraform/log/variables.tf
@@ -5,7 +5,7 @@ variable "environment" {
   type = string
 }
 variable "env_type" {
-  type    = string
+  type = string
   validation {
     condition     = var.env_type == "prd" || var.env_type == "npd"
     error_message = "environment must be either prd or npd"
