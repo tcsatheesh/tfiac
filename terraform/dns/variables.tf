77d0b1e71899aa527298836e4e1b4dd4723946e8
Satheesh TC <tc.satheesh@microsoft.com>
2025-03-11 17:16:35 +0000

    format

--- terraform/dns/variables.tf
+++ terraform/dns/variables.tf
@@ -5,7 +5,7 @@ variable "environment" {
   type = string
 }
 variable "env_type" {
-  type    = string
+  type = string
   validation {
     condition     = var.env_type == "prd"
     error_message = "env_type must be either prd or pre or dev"
--- terraform/services/variables.tf
+++ terraform/services/variables.tf
@@ -5,7 +5,7 @@ variable "environment" {
   type = string
 }
 variable "env_type" {
-  type    = string
+  type = string
   validation {
     condition     = var.env_type == "prd" || var.env_type == "pre" || var.env_type == "dev"
     error_message = "env_type must be either prd or pre or dev"
