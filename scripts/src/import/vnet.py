5910b5a612682a2ca00e1e8b175285ffd6d3516c
Satheesh TC <tc.satheesh@microsoft.com>
2025-03-12 23:57:30 +0000

    remote vnet support

--- scripts/src/import/vnet.py
+++ scripts/src/import/vnet.py
@@ -100,12 +100,32 @@ class ImportState:
                 resource_id=f'/subscriptions/{_vnet_subscription_id}/resourceGroups/{_vnet_resource_group_name}/providers/Microsoft.Network/virtualNetworks/{_vnet_variables["name"]}|sendToLogAnalytics',
             )
 
-
-
-            self._import_resource(
-                name='module.vnet.module.peering["enabled"].azapi_resource.this[0]',
-                resource_id=f'/subscriptions/{_vnet_subscription_id}/resourceGroups/{_vnet_resource_group_name}/providers/Microsoft.Network/virtualNetworks/{_vnet_variables["name"]}/virtualNetworkPeerings/{_vnet_variables["vnet_peering"]["local_name"]}',
+            _remote_vnet_variables_file_path = os.path.join(
+                os.path.abspath(os.getcwd()),
+                _args.remote_vnet_variables,
             )
+            _remote_vnet_variables_file_path = os.path.abspath(
+                _remote_vnet_variables_file_path,
+            )
+
+            if not os.path.exists(_remote_vnet_variables_file_path):
+                self._logger.error(f"Remote VNet variables file not found at {_remote_vnet_variables_file_path}")
+            else:
+                self._logger.info(f"Importing Remote VNet variables from {_remote_vnet_variables_file_path}")
+                with open(_remote_vnet_variables_file_path, "r") as file:
+                    _remote_vnet_variables = yaml.safe_load(file)
+
+                _remote_vnet_subscription_id = _remote_vnet_variables["subscription_id"]
+                _remote_vnet_resource_group_name = _remote_vnet_variables["resource_group_name"]
+
+                self._import_resource(
+                    name='module.vnet.module.peering["enabled"].azapi_resource.this[0]',
+                    resource_id=f'/subscriptions/{_vnet_subscription_id}/resourceGroups/{_vnet_resource_group_name}/providers/Microsoft.Network/virtualNetworks/{_vnet_variables["name"]}/virtualNetworkPeerings/{_vnet_variables["vnet_peering"]["local_name"]}',
+                )
+                self._import_resource(
+                    name='module.vnet.module.peering["enabled"].azapi_resource.reverse[0]',
+                    resource_id=f'/subscriptions/{_remote_vnet_subscription_id}/resourceGroups/{_remote_vnet_resource_group_name}/providers/Microsoft.Network/virtualNetworks/{_remote_vnet_variables["name"]}/virtualNetworkPeerings/{_vnet_variables["vnet_peering"]["remote_name"]}',
+                )
 
 
 
@@ -157,6 +177,13 @@ if __name__ == "__main__":
         type=str,
         required=False,
         help="DNS variables to use",
+        default="variables/grp/prd/dns.yaml",
+    )
+    parser.add_argument(
+        "--remote-vnet-variables",
+        type=str,
+        required=False,
+        help="Remote VNet variables to use",
         default="variables/grp/prd/dns.yaml",
     )
     parser.add_argument(
