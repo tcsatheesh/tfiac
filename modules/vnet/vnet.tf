c572714bb67889e99166f7782f0c8f6274b07703
Satheesh TC <tc.satheesh@microsoft.com>
2025-03-12 00:50:48 +0000

    add resource group

--- modules/vnet/vnet.tf
+++ modules/vnet/vnet.tf
@@ -23,10 +23,15 @@ data "azurerm_log_analytics_workspace" "this" {
   resource_group_name = var.log.resource_group_name
 }
 
+resource "azurerm_resource_group" "this" {
+  name     = var.vnet.resource_group_name
+  location = var.vnet.location
+}
+
 resource "azurerm_route_table" "this" {
   location            = var.vnet.location
   name                = var.vnet.route_table_name
-  resource_group_name = var.vnet.resource_group_name
+  resource_group_name = azurerm_resource_group.this.name
 
   route {
     name           = "firewall-appliance"
@@ -41,7 +46,7 @@ resource "azurerm_network_security_group" "subnet" {
 
   location            = var.vnet.location
   name                = each.value.nsg
-  resource_group_name = var.vnet.resource_group_name
+  resource_group_name = azurerm_resource_group.this.name
 }
 
 
@@ -49,7 +54,7 @@ resource "azurerm_network_security_group" "subnet" {
 module "vnet" {
   source              = "Azure/avm-res-network-virtualnetwork/azurerm"
   name                = var.vnet.name
-  resource_group_name = var.vnet.resource_group_name
+  resource_group_name = azurerm_resource_group.this.name
   location            = var.vnet.location
 
   address_space = var.vnet.address_space
