9bd9cf11c6a5046ef2b64b459b52ebd3af747959
Satheesh TC <tc.satheesh@microsoft.com>
2025-03-13 18:00:02 +0000

    test

--- terraform/services/main.tf
+++ terraform/services/main.tf
@@ -69,98 +69,98 @@ module "keyvault" {
   }
 }
 
-module "appinsights" {
-  source   = "../../modules/appinsights"
-  dns      = local.dns
-  log      = local.log
-  vnet     = local.vnet
-  services = local.services
-}
+# module "appinsights" {
+#   source   = "../../modules/appinsights"
+#   dns      = local.dns
+#   log      = local.log
+#   vnet     = local.vnet
+#   services = local.services
+# }
 
-module "landing_zone" {
-  source       = "../../modules/storage"
-  dns          = local.dns
-  log          = local.log
-  vnet         = local.vnet
-  services     = local.services
-  storage_type = "landing"
-}
+# module "landing_zone" {
+#   source       = "../../modules/storage"
+#   dns          = local.dns
+#   log          = local.log
+#   vnet         = local.vnet
+#   services     = local.services
+#   storage_type = "landing"
+# }
 
-module "aiservices" {
-  source   = "../../modules/aiservices"
-  dns      = local.dns
-  log      = local.log
-  vnet     = local.vnet
-  services = local.services
-}
+# module "aiservices" {
+#   source   = "../../modules/aiservices"
+#   dns      = local.dns
+#   log      = local.log
+#   vnet     = local.vnet
+#   services = local.services
+# }
 
-module "openai" {
-  source   = "../../modules/openai"
-  dns      = local.dns
-  log      = local.log
-  vnet     = local.vnet
-  services = local.services
-}
+# module "openai" {
+#   source   = "../../modules/openai"
+#   dns      = local.dns
+#   log      = local.log
+#   vnet     = local.vnet
+#   services = local.services
+# }
 
-module "docint" {
-  source   = "../../modules/docint"
-  dns      = local.dns
-  log      = local.log
-  vnet     = local.vnet
-  services = local.services
-}
+# module "docint" {
+#   source   = "../../modules/docint"
+#   dns      = local.dns
+#   log      = local.log
+#   vnet     = local.vnet
+#   services = local.services
+# }
 
-module "language" {
-  source   = "../../modules/language"
-  dns      = local.dns
-  log      = local.log
-  vnet     = local.vnet
-  services = local.services
-}
+# module "language" {
+#   source   = "../../modules/language"
+#   dns      = local.dns
+#   log      = local.log
+#   vnet     = local.vnet
+#   services = local.services
+# }
 
-module "cntreg" {
-  source   = "../../modules/cntreg"
-  dns      = local.dns
-  log      = local.log
-  vnet     = local.vnet
-  services = local.services
-}
+# module "cntreg" {
+#   source   = "../../modules/cntreg"
+#   dns      = local.dns
+#   log      = local.log
+#   vnet     = local.vnet
+#   services = local.services
+# }
 
-module "aml" {
-  source                = "../../modules/aml"
-  dns                   = local.dns
-  log                   = local.log
-  vnet                  = local.vnet
-  services              = local.services
-  app_insights_id       = module.appinsights.app_insights_id
-  keyvault_id           = module.keyvault.keyvault_id
-  container_registry_id = module.cntreg.container_registry_id
-}
+# module "aml" {
+#   source                = "../../modules/aml"
+#   dns                   = local.dns
+#   log                   = local.log
+#   vnet                  = local.vnet
+#   services              = local.services
+#   app_insights_id       = module.appinsights.app_insights_id
+#   keyvault_id           = module.keyvault.keyvault_id
+#   container_registry_id = module.cntreg.container_registry_id
+# }
 
-module "function_app" {
-  source                           = "../../modules/fnapp"
-  dns                              = local.dns
-  log                              = local.log
-  vnet                             = local.vnet
-  services                         = local.services
-  app_insights_instrumentation_key = module.appinsights.instrumentation_key
-  app_insights_connection_string   = module.appinsights.connection_string
-}
+# module "function_app" {
+#   source                           = "../../modules/fnapp"
+#   dns                              = local.dns
+#   log                              = local.log
+#   vnet                             = local.vnet
+#   services                         = local.services
+#   app_insights_instrumentation_key = module.appinsights.instrumentation_key
+#   app_insights_connection_string   = module.appinsights.connection_string
+# }
 
-module "search" {
-  source   = "../../modules/search"
-  dns      = local.dns
-  log      = local.log
-  vnet     = local.vnet
-  services = local.services
-}
+# module "search" {
+#   source   = "../../modules/search"
+#   dns      = local.dns
+#   log      = local.log
+#   vnet     = local.vnet
+#   services = local.services
+# }
 
-module "logic_app" {
-  source                           = "../../modules/lgapp"
-  dns                              = local.dns
-  log                              = local.log
-  vnet                             = local.vnet
-  services                         = local.services
-  app_insights_instrumentation_key = module.appinsights.instrumentation_key
-  app_insights_connection_string   = module.appinsights.connection_string
-}
+# module "logic_app" {
+#   source                           = "../../modules/lgapp"
+#   dns                              = local.dns
+#   log                              = local.log
+#   vnet                             = local.vnet
+#   services                         = local.services
+#   app_insights_instrumentation_key = module.appinsights.instrumentation_key
+#   app_insights_connection_string   = module.appinsights.connection_string
+# }
