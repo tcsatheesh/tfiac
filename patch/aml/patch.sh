2b11b4912e5307235c4d54316313f87a9d6e7b0e
Satheesh TC <tc.satheesh@microsoft.com>
2025-03-15 14:21:18 +0000

    patch aml name

--- patch/aml/patch.sh
+++ patch/aml/patch.sh
@@ -1,6 +1,18 @@
 echo "Patching AzureML Private Endpoint"
-file_to_patch="./terraform/services/.terraform/modules//aml.azureml/main.privateendpoint.tf"
+file_to_patch="./terraform/services/.terraform/modules/aml.azureml/main.privateendpoint.tf"
 input_file="./patch/aml/pe.patch"
+if [ ! -f $file_to_patch ]; then
+    echo "File $file_to_patch does not exist."
+    exit 1
+fi
+echo "Applying patch to $file_to_patch"
+echo "Apply patch from $input_file"
+patch -u $file_to_patch -i $input_file
+echo "Patch applied successfully"
+
+echo "Patching AzureML Name"
+file_to_patch="./terraform/services/.terraform/modules/aml.azureml/main.tf"
+input_file="./patch/aml/main.patch"
 if [ ! -f $file_to_patch ]; then
     echo "File $file_to_patch does not exist."
     exit 1
