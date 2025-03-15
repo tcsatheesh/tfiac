a0ae091245e7c16f9440d977d24185cc48e62106
Satheesh TC <tc.satheesh@microsoft.com>
2025-03-15 22:58:18 +0000

    reformat

--- modules/vnet/rulecollections.tf
+++ modules/vnet/rulecollections.tf
@@ -3,14 +3,12 @@ module "denyall" {
   firewall_policy_id = module.fwpolicy[0].resource_id
 }
 
-locals {
-  azuread = [for _key in var.firewall.rulecollections.azuread :
-    azurerm_ip_group.this[_key].id
-  ]
-}
 
 module "azuread" {
-  source             = "./rulecollection/azuread"
-  source_ip_groups   = local.azuread
+  source = "./rulecollection/azuread"
+  source_ip_groups = [for _key in var.firewall.rulecollections.azuread :
+    azurerm_ip_group.this[_key].id
+  ]
   firewall_policy_id = module.fwpolicy[0].resource_id
 }
+
