bbd0c41a2713114efdb1440fe968920c7cdd93fc
Satheesh TC <tc.satheesh@microsoft.com>
2025-03-16 23:46:26 +0000

    apim policy support

--- modules/apim/api/main.tf
+++ modules/apim/api/main.tf
@@ -61,3 +61,86 @@ resource "azurerm_api_management_api_operation" "redactpii" {
   url_template        = "/language/:analyze-text?api-version=2022-05-01"
   description         = "Redact the PII for the given text."
 }
+
+resource "azurerm_api_management_api_policy" "openai" {
+  api_name            = azurerm_api_management_api.openai.name
+  api_management_name = var.services.apim.name
+  resource_group_name = var.services.apim.resource_group_name
+
+  xml_content = <<XML
+<policies>
+    <inbound>
+        <base />
+        <authentication-managed-identity resource="https://cognitiveservices.azure.com" />
+    </inbound>
+    <backend>
+        <base />
+    </backend>
+    <outbound>
+        <base />
+        <set-header name="x-openai-backend-id" exists-action="override">
+            <value>${var.services.apim.apis.openai.backend}</value>
+        </set-header>
+    </outbound>
+    <on-error>
+        <base />
+    </on-error>
+</policies>
+XML
+}
+
+resource "azurerm_api_management_api_policy" "monitoring" {
+  api_name            = azurerm_api_management_api.monitoring.name
+  api_management_name = var.services.apim.name
+  resource_group_name = var.services.apim.resource_group_name
+
+  xml_content = <<XML
+<policies>
+    <inbound>
+        <base />
+        <authentication-managed-identity resource="https://cognitiveservices.azure.com" />
+    </inbound>
+    <backend>
+        <base />
+    </backend>
+    <outbound>
+        <base />
+        <set-header name="x-openai-backend-id" exists-action="override">
+            <value>${var.services.apim.apis.monitoring.backend}</value>
+        </set-header>
+    </outbound>
+    <on-error>
+        <base />
+    </on-error>
+</policies>
+XML
+}
+
+resource "azurerm_api_management_api_policy" "ailanguage" {
+  api_name            = azurerm_api_management_api.ailanguage.name
+  api_management_name = var.services.apim.name
+  resource_group_name = var.services.apim.resource_group_name
+
+  xml_content = <<XML
+<policies>
+    <!-- Throttle, authorize, validate, cache, or transform the requests -->
+    <inbound>
+        <authentication-managed-identity resource="https://cognitiveservices.azure.com/" />
+        <base />
+    </inbound>
+    <!-- Control if and how the requests are forwarded to services  -->
+    <backend>
+        <base />
+    </backend>
+    <!-- Customize the responses -->
+    <outbound>
+        <base />
+    </outbound>
+    <!-- Handle exceptions and customize error responses  -->
+    <on-error>
+        <base />
+    </on-error>
+</policies>
+XML
+}
+
